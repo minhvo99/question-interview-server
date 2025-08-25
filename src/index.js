77d2f0a0837e1a1bc5dc0dd6513f91ebe39ca564
minhvo99 <pikachu43dn@gmail.com>
2025-08-25 08:48:11 +0700

    update vercel config, refactor server

--- src/index.js
+++ src/index.js
@@ -2,17 +2,12 @@ import express from 'express';
 import { config } from 'dotenv';
 import morgan from 'morgan';
 import db from './config/database.js';
-import helmet from 'helmet';
 import appRouter from './routes/index.js';
 import { errorHandler } from './controllers/Error.controller.js';
 import { rateLimit } from 'express-rate-limit';
 import cors from 'cors';
-import session from 'express-session';
-import mongoSanitize from 'express-mongo-sanitize';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import logger from './logger/winston.js';
-import AppError from './utils/appError.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -22,8 +17,6 @@ await db.connect();
 
 const app = express();
 
-// Set security HHTP headers
-app.use(helmet());
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
@@ -36,57 +29,48 @@ const limiter = rateLimit({
   message: 'Too many request from this IP, please try again in an hour!',
 });
 
-// config session
-
 app.use(
   cors({
     origin: ['http://localhost:4200', 'https://question-interview.vercel.app/'],
     credentials: true,
   }),
 );
-// app.use(
-//   session({
-//     secret: process.env.SESSION_SECRET || '',
-//     resave: false,
-//     saveUninitialized: true,
-//   }),
-// );
-
-// Passport middleware
 
 app.use('/api', limiter);
 app.use(express.json({ limit: '100kb' }));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
-// Data sanitization against NoSQL query injection
-// app.use(mongoSanitize());
 app.use(express.static(`${__dirname}/assets`));
 
 app.use('/api/v1', appRouter);
 
 // Global error
 app.use(errorHandler);
+const server = http.createServer(app);
+server.listen(port, () => {
+  console.log(`App listening on port http://localhost:${port}/`);
+});
 
-export default app;
+// export default app;
 
-if (process.argv[1] === new URL(import.meta.url).pathname) {
-  const port = process.env.PORT || 8080;
-  const server = app.listen(port, () => {
-    console.log(`App listening on port http://localhost:${port}/`);
-  });
-  process.on('unhandledRejection', (err) => {
-    console.log('UNHANDLED Rejection! 💣 Shutting down...');
-    console.log(err.name, err.message);
-    server.close(() => {
-      process.exit(1);
-    });
-  });
+// if (process.argv[1] === new URL(import.meta.url).pathname) {
+//   const port = process.env.PORT || 8080;
+//   app.listen(port, () => {
+//     console.log(`App listening on port http://localhost:${port}/`);
+//   });
+//   process.on('unhandledRejection', (err) => {
+//     console.log('UNHANDLED Rejection! 💣 Shutting down...');
+//     console.log(err.name, err.message);
+//     server.close(() => {
+//       process.exit(1);
+//     });
+//   });
 
-  process.on('uncaughtException', (err) => {
-    console.log('UNCAUGHT Exception! 💣 Shutting down...');
-    console.log(err.name, err.message);
-    server.close(() => {
-      process.exit(1);
-    });
-  });
-}
+//   process.on('uncaughtException', (err) => {
+//     console.log('UNCAUGHT Exception! 💣 Shutting down...');
+//     console.log(err.name, err.message);
+//     server.close(() => {
+//       process.exit(1);
+//     });
+//   });
+// }
