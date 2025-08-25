24fac9c52f7cc07f2f3ff0f2c15763cee7917f44
minhvo99 <pikachu43dn@gmail.com>
2025-08-25 08:37:10 +0700

    Revert "fix issue vercel"
    This reverts commit 5a297db553b067b2153789e9154fa65bf45016bc.

--- src/index.js
+++ src/index.js
@@ -69,24 +69,24 @@ app.use(errorHandler);
 
 export default app;
 
-// if (process.argv[1] === new URL(import.meta.url).pathname) {
-//   const port = process.env.PORT || 8080;
-//   const server = app.listen(port, () => {
-//     console.log(`App listening on port http://localhost:${port}/`);
-//   });
-//   process.on('unhandledRejection', (err) => {
-//     console.log('UNHANDLED Rejection! 💣 Shutting down...');
-//     console.log(err.name, err.message);
-//     server.close(() => {
-//       process.exit(1);
-//     });
-//   });
+if (process.argv[1] === new URL(import.meta.url).pathname) {
+  const port = process.env.PORT || 8080;
+  const server = app.listen(port, () => {
+    console.log(`App listening on port http://localhost:${port}/`);
+  });
+  process.on('unhandledRejection', (err) => {
+    console.log('UNHANDLED Rejection! 💣 Shutting down...');
+    console.log(err.name, err.message);
+    server.close(() => {
+      process.exit(1);
+    });
+  });
 
-//   process.on('uncaughtException', (err) => {
-//     console.log('UNCAUGHT Exception! 💣 Shutting down...');
-//     console.log(err.name, err.message);
-//     server.close(() => {
-//       process.exit(1);
-//     });
-//   });
-// }
+  process.on('uncaughtException', (err) => {
+    console.log('UNCAUGHT Exception! 💣 Shutting down...');
+    console.log(err.name, err.message);
+    server.close(() => {
+      process.exit(1);
+    });
+  });
+}
